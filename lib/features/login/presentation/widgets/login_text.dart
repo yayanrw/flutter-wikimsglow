f534f53cf48551466977c4736f707741ed40ef3e
yayanrw <yayanraw@gmail.com>
2022-03-29 19:48:46 +0700

    login text

--- lib/features/login/presentation/widgets/login_text.dart
+++ lib/features/login/presentation/widgets/login_text.dart
@@ -3,16 +3,9 @@ import 'package:wikimsglow/core/theme/color_themes.dart';
 import 'package:wikimsglow/core/theme/text_themes.dart';
 import 'package:wikimsglow/core/utils/strings.dart';
 
-class LoginText extends StatelessWidget {
-  const LoginText({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      Strings.loginToYourAccount,
-      style: textThemes(ColorTheme.primary, FontWeight.w700).headline4,
-    );
-  }
+Text loginText() {
+  return Text(
+    Strings.loginToYourAccount,
+    style: textThemes(ColorTheme.primary, FontWeight.w700).headline4,
+  );
 }
