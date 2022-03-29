33c3e19b9cfac88600114166e88336911747055d
yayanrw <yayanraw@gmail.com>
2022-03-30 05:26:36 +0700

    add button callback

--- lib/features/login/presentation/widgets/forgot_password.dart
+++ lib/features/login/presentation/widgets/forgot_password.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:wikimsglow/core/theme/color_themes.dart';
 import 'package:wikimsglow/core/theme/text_themes.dart';
 import 'package:wikimsglow/core/utils/strings.dart';
+import 'package:wikimsglow/features/others/presentation/pages/under_development.dart';
 
-TextButton forgotPassword() {
+TextButton forgotPassword(BuildContext context) {
   return TextButton(
-      onPressed: () {},
+      onPressed: () => Navigator.pushNamed(context, UnderDevelopment.routeName),
       child: Text(Strings.forgotPassword,
           style: textThemes(ColorTheme.primary, FontWeight.w500).bodyText1));
 }
