d0c0f739f720219c7db87a64b353b1ab49243aff
yayanrw <yayanraw@gmail.com>
2022-03-29 14:02:14 +0700

    add login pages

---
+++ lib/features/login/presentation/pages/login_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  static const routeName = '/login';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('LoginPage');
+  }
+}
