75cc4b0754edf0f1b6ee194076737bbe9b6de88c
yayanrw <yayanraw@gmail.com>
2022-03-29 17:02:55 +0700

    add strings

---
+++ lib/core/utils/strings.dart
@@ -0,0 +1,12 @@
+class Strings {
+  static const email = 'Email';
+  static const register = 'Register';
+  static const dontHaveAccount = 'Don\'t have an account?';
+  static const loginWithGoogle = 'Login with Google';
+  static const or = 'OR';
+  static const forgotPassword = 'Forgot Password';
+  static const login = 'Login';
+  static const password = 'Password';
+  static const rememberMe = 'Remember me';
+  static const loginToYourAccount = 'Login to your\naccount';
+}
