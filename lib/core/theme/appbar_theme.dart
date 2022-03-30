524f11b7648d0ebe180ad7b936fc75bd5ed6bc9c
yayanrw <yayanraw@gmail.com>
2022-03-31 04:49:38 +0700

    transparent appbar

--- lib/core/theme/appbar_theme.dart
+++ lib/core/theme/appbar_theme.dart
@@ -7,6 +7,6 @@ AppBarTheme appBarTheme() {
     elevation: 0,
     systemOverlayStyle: SystemUiOverlayStyle.light,
     iconTheme: IconThemeData(color: ColorTheme.primary),
-    backgroundColor: Colors.white,
+    backgroundColor: Colors.transparent,
   );
 }
