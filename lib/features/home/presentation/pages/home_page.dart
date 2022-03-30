24cd0a4e3bd68009f34353e6845599826e12b6a9
yayanrw <yayanraw@gmail.com>
2022-03-31 04:46:36 +0700

    cleaning home

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:wikimsglow/core/config/apps_config.dart';
-import 'package:wikimsglow/core/theme/color_themes.dart';
-import 'package:wikimsglow/core/theme/text_themes.dart';
 import 'package:wikimsglow/core/utils/strings.dart';
 import 'package:wikimsglow/core/widgets/text/section_title.dart';
 import 'package:wikimsglow/features/home/presentation/widgets/app_bar_home_page.dart';
-import 'package:wikimsglow/features/home/presentation/widgets/categories_card.dart';
+import 'package:wikimsglow/features/home/presentation/widgets/explore_text.dart';
 import 'package:wikimsglow/features/home/presentation/widgets/search_text_form_field.dart';
 import 'package:wikimsglow/features/others/presentation/pages/under_development.dart';
 
@@ -33,9 +30,6 @@ class HomePage extends StatelessWidget {
                 press: () =>
                     Navigator.pushNamed(context, UnderDevelopment.routeName),
               ),
-              const SizedBox(height: 8),
-              categoriesSection(),
-              const SizedBox(height: 16),
               SectionTitle(
                 text: Strings.ourProducts,
                 isMoreable: false,
@@ -44,43 +38,6 @@ class HomePage extends StatelessWidget {
             ],
           ),
         ),
-      ),
-    );
-  }
-
-  Padding exploreText() {
-    return Padding(
-        padding: const EdgeInsets.all(AppsConfig.defaultPadding),
-        child: RichText(
-          text: TextSpan(
-              text: Strings.explore,
-              style:
-                  textThemes(ColorTheme.textBlack, FontWeight.w600).headline4,
-              children: [
-                TextSpan(
-                    text: Strings.ourSkinCare,
-                    style: textThemes(ColorTheme.textGrey, FontWeight.w500)
-                        .headline6)
-              ]),
-        ));
-  }
-
-  SizedBox categoriesSection() {
-    return SizedBox(
-      height: 120,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        shrinkWrap: true,
-        itemCount: 5,
-        itemBuilder: (context, index) {
-          return CategoriesCard(
-            icons: Icons.coffee,
-            title: 'Skincare $index',
-            press: () =>
-                Navigator.pushNamed(context, UnderDevelopment.routeName),
-            index: index,
-          );
-        },
       ),
     );
   }
