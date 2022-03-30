f37417856f10b812c2761f7dc61949acfe2a7bd0
yayanrw <yayanraw@gmail.com>
2022-03-31 03:52:38 +0700

    add leading appbar

--- lib/features/home/presentation/widgets/app_bar_home_page.dart
+++ lib/features/home/presentation/widgets/app_bar_home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:wikimsglow/core/theme/text_themes.dart';
 import 'package:wikimsglow/core/utils/strings.dart';
 
@@ -6,6 +7,10 @@ AppBar appBarHome() {
   return AppBar(
     automaticallyImplyLeading: false,
     centerTitle: true,
+    leading: IconButton(
+      icon: SvgPicture.asset('assets/icons/menu.svg'),
+      onPressed: () {},
+    ),
     title: RichText(
       text: TextSpan(
         text: Strings.wiki,
