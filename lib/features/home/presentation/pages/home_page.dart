e9f9eb37284ac581676dbe893403b84b7c09f965
yayanrw <yayanraw@gmail.com>
2022-03-30 10:12:04 +0700

    add homepage appbar

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wikimsglow/core/theme/text_themes.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -7,8 +8,26 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(child: Center(child: Text('Home Page'))),
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        centerTitle: true,
+        title: RichText(
+          text: TextSpan(
+            text: 'wiki',
+            style: textThemes(Colors.black, FontWeight.w400).headline4,
+            children: [
+              TextSpan(
+                  text: 'msglow',
+                  style: textThemes(Colors.black, FontWeight.w900).headline4),
+            ],
+          ),
+        ),
+      ),
+      body: SafeArea(
+          child: SingleChildScrollView(
+        child: Text('Home Page'),
+      )),
     );
   }
 }
