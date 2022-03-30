f89c70e432bb23a3539e45b9d2cd1ea65fdd7730
yayanrw <yayanraw@gmail.com>
2022-03-30 08:35:02 +0700

    add home page

---
+++ lib/features/home/presentation/pages/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  static const routeName = '/home';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(child: Center(child: Text('Home Page'))),
+    );
+  }
+}
