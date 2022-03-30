6351e8d9f0d16929010651fed4dc92494649602d
yayanrw <yayanraw@gmail.com>
2022-03-31 04:36:06 +0700

    add default padding

--- lib/features/home/presentation/widgets/search_text_form_field.dart
+++ lib/features/home/presentation/widgets/search_text_form_field.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:wikimsglow/core/config/apps_config.dart';
 import 'package:wikimsglow/core/theme/input_decoration_theme.dart';
 
 Widget searchTextFormField() {
   return Padding(
-    padding: const EdgeInsets.symmetric(horizontal: 16),
+    padding: const EdgeInsets.all(AppsConfig.defaultPadding),
     child: TextFormField(
       decoration: searchInputDecoration(),
     ),
