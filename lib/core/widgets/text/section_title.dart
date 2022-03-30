d8fbf97f300e579f999f20f705d9b5361c1be038
yayanrw <yayanraw@gmail.com>
2022-03-30 10:58:22 +0700

    add section title widg

---
+++ lib/core/widgets/text/section_title.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:wikimsglow/core/theme/color_themes.dart';
+import 'package:wikimsglow/core/theme/text_themes.dart';
+
+class SectionTitle extends StatelessWidget {
+  const SectionTitle({
+    Key? key,
+    required this.text,
+    required this.press,
+    required this.isMoreable,
+  }) : super(key: key);
+
+  final String text;
+  final bool isMoreable;
+  final VoidCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            text,
+            style: textThemes(ColorTheme.secondary, FontWeight.w700).headline6,
+          ),
+          Visibility(
+            visible: isMoreable,
+            child: GestureDetector(
+                onTap: press,
+                child: Text(
+                  'See more',
+                  style: textThemes(ColorTheme.textGrey).caption,
+                )),
+          )
+        ],
+      ),
+    );
+  }
+}
