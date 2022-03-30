67507584efc84b522dd20c82c3b5f8acac5d0e9d
yayanrw <yayanraw@gmail.com>
2022-03-30 10:46:18 +0700

    cleaning

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:wikimsglow/core/theme/text_themes.dart';
-import 'package:wikimsglow/core/utils/strings.dart';
 import 'package:wikimsglow/features/home/presentation/widgets/app_bar_home_page.dart';
 import 'package:wikimsglow/features/home/presentation/widgets/search_text_form_field.dart';
 
--- lib/features/login/presentation/pages/login_page.dart
+++ lib/features/login/presentation/pages/login_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:wikimsglow/core/theme/color_themes.dart';
 import 'package:wikimsglow/core/theme/input_decoration_theme.dart';
 import 'package:wikimsglow/core/utils/strings.dart';
-import 'package:wikimsglow/core/utils/validations.dart';
 import 'package:wikimsglow/core/widgets/button/primary_button.dart';
 import 'package:wikimsglow/features/home/presentation/pages/home_page.dart';
 import 'package:wikimsglow/features/login/presentation/widgets/dont_have_account.dart';
