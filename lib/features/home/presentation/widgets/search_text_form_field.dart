229837669d486cfe2d777096c0bea30129964155
yayanrw <yayanraw@gmail.com>
2022-03-30 10:58:34 +0700

    change padding

--- lib/features/home/presentation/widgets/search_text_form_field.dart
+++ lib/features/home/presentation/widgets/search_text_form_field.dart
@@ -3,7 +3,7 @@ import 'package:wikimsglow/core/theme/input_decoration_theme.dart';
 
 Widget searchTextFormField() {
   return Padding(
-    padding: const EdgeInsets.symmetric(horizontal: 24),
+    padding: const EdgeInsets.symmetric(horizontal: 16),
     child: TextFormField(
       decoration: searchInputDecoration(),
     ),
