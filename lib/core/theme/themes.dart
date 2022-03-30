e4b2ed6de6eb8b83681a6e7af684a28d6a3377f1
yayanrw <yayanraw@gmail.com>
2022-03-30 09:50:43 +0700

    mod theme

--- lib/core/theme/themes.dart
+++ lib/core/theme/themes.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:wikimsglow/core/config/apps_config.dart';
+import 'package:wikimsglow/core/theme/appbar_theme.dart';
 import 'package:wikimsglow/core/theme/color_themes.dart';
+import 'package:wikimsglow/core/theme/text_selection_theme.dart';
 
 ThemeData themes() {
   return ThemeData(
@@ -9,6 +11,8 @@ ThemeData themes() {
       primaryTextTheme: const TextTheme(
         headline6: TextStyle(color: Colors.black),
       ),
+      textSelectionTheme: textSelectionThemeData(),
+      appBarTheme: appBarTheme(),
       fontFamily: AppsConfig.fontFamily,
       visualDensity: VisualDensity.adaptivePlatformDensity);
 }
