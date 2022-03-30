05343f2a3c8e804d350ab54065fc44b0e642b23d
yayanrw <yayanraw@gmail.com>
2022-03-31 04:59:53 +0700

    add category model

---
+++ lib/features/home/domain/entities/category.dart
@@ -0,0 +1,28 @@
+class Category {
+  final String icon, title;
+
+  Category({required this.icon, required this.title});
+}
+
+List<Category> demo_categories = [
+  Category(
+    icon: "assets/icons/dress.svg",
+    title: "Skincare",
+  ),
+  Category(
+    icon: "assets/icons/shirt.svg",
+    title: "Bodycare",
+  ),
+  Category(
+    icon: "assets/icons/pants.svg",
+    title: "MS Slim",
+  ),
+  Category(
+    icon: "assets/icons/Tshirt.svg",
+    title: "Cosmetics",
+  ),
+  Category(
+    icon: "assets/icons/Tshirt.svg",
+    title: "Beauty Tools",
+  ),
+];
