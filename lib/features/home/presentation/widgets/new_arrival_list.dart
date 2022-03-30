444f678578ce31c5d7b301e69f8b13605771b89b
yayanrw <yayanraw@gmail.com>
2022-03-31 06:54:15 +0700

    add arrival list

---
+++ lib/features/home/presentation/widgets/new_arrival_list.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:wikimsglow/core/config/apps_config.dart';
+import 'package:wikimsglow/features/home/domain/entities/product.dart';
+
+import 'product_card.dart';
+
+class NewArrivalList extends StatelessWidget {
+  const NewArrivalList({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: AppsConfig.defaultPadding / 2,
+        ),
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(
+              parent: AlwaysScrollableScrollPhysics()),
+          scrollDirection: Axis.horizontal,
+          child: ListView.separated(
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              itemCount: demoNewArrivals.length,
+              separatorBuilder: (context, index) =>
+                  const SizedBox(width: AppsConfig.defaultPadding / 2),
+              itemBuilder: (context, index) {
+                bool isFirstIndex = index == 0;
+                bool isLastIndex = index == demoNewArrivals.length - 1;
+                return ProductCard(
+                  title: demoNewArrivals[index].title,
+                  image: demoNewArrivals[index].image,
+                  press: () {},
+                  isFirstIndex: isFirstIndex,
+                  isLastIndex: isLastIndex,
+                );
+              }),
+        ),
+      ),
+    );
+  }
+}
