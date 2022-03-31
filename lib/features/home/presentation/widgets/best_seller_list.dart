d92e754be055d6158a3e67af7b6153dc8fb957e1
yayanrw <yayanraw@gmail.com>
2022-03-31 07:14:07 +0700

    best seller list

---
+++ lib/features/home/presentation/widgets/best_seller_list.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:wikimsglow/core/config/apps_config.dart';
+import 'package:wikimsglow/features/home/domain/entities/product.dart';
+
+import 'product_card.dart';
+
+class BestSellerList extends StatelessWidget {
+  const BestSellerList({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: AppsConfig.defaultPadding / 2,
+        ),
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(
+              parent: AlwaysScrollableScrollPhysics()),
+          scrollDirection: Axis.horizontal,
+          child: ListView.separated(
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              itemCount: demoBestSellers.length,
+              separatorBuilder: (context, index) =>
+                  const SizedBox(width: AppsConfig.defaultPadding / 2),
+              itemBuilder: (context, index) {
+                bool isFirstIndex = index == 0;
+                bool isLastIndex = index == demoBestSellers.length - 1;
+                return ProductCard(
+                  title: demoBestSellers[index].title,
+                  image: demoBestSellers[index].image,
+                  press: () {},
+                  isFirstIndex: isFirstIndex,
+                  isLastIndex: isLastIndex,
+                );
+              }),
+        ),
+      ),
+    );
+  }
+}
