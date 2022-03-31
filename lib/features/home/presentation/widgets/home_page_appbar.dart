52aaf2a2c3a4f762c442e4dd5de31d709d248a25
yayanrw <yayanraw@gmail.com>
2022-03-31 15:59:38 +0700

    change color appbar

--- lib/features/home/presentation/widgets/home_page_appbar.dart
+++ lib/features/home/presentation/widgets/home_page_appbar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:wikimsglow/core/theme/color_themes.dart';
 import 'package:wikimsglow/core/theme/text_themes.dart';
 import 'package:wikimsglow/core/utils/strings.dart';
 
@@ -8,17 +9,20 @@ AppBar appBarHome() {
     automaticallyImplyLeading: false,
     centerTitle: true,
     leading: IconButton(
-      icon: SvgPicture.asset('assets/icons/menu.svg'),
+      icon: SvgPicture.asset(
+        'assets/icons/menu.svg',
+        color: ColorTheme.primary,
+      ),
       onPressed: () {},
     ),
     title: RichText(
       text: TextSpan(
         text: Strings.wiki,
-        style: textThemes(Colors.black, FontWeight.w400).headline4,
+        style: textThemes(ColorTheme.primary, FontWeight.w400).headline4,
         children: [
           TextSpan(
               text: Strings.msglow,
-              style: textThemes(Colors.black, FontWeight.w900).headline4),
+              style: textThemes(ColorTheme.primary, FontWeight.w900).headline4),
         ],
       ),
     ),
