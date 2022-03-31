e43dc7bdb66f7f32d9917b9f9c803dff9438475f
yayanrw <yayanraw@gmail.com>
2022-03-31 09:01:21 +0700

    remove material

--- lib/features/home/presentation/pages/product_detail_page.dart
+++ lib/features/home/presentation/pages/product_detail_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:wikimsglow/core/config/apps_config.dart';
-import 'package:wikimsglow/core/theme/color_themes.dart';
 import 'package:wikimsglow/features/home/domain/entities/product.dart';
 
 class ProductDetailPage extends StatelessWidget {
