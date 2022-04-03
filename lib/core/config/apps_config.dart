e78fecf81a78f3a4f9f2f5ffc07a586b61aa277e
yayanrw <yayanraw@gmail.com>
2022-04-03 18:41:13 +0700

    add base url

--- lib/core/config/apps_config.dart
+++ lib/core/config/apps_config.dart
@@ -7,4 +7,5 @@ class AppsConfig {
   static const defaultPadding = 16.0;
   static const defaultRadius = 16.0;
   static final defaultTextColor = MyColors.textBlack;
+  static const baseUrl = 'http://103.157.96.97/codeigniter3-jwtauth/';
 }
