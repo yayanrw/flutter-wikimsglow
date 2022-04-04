09caccf978d0c6fc23715ca88ef0ec9987f7bb9d
yayanrw <yayanraw@gmail.com>
2022-04-04 11:49:53 +0700

    multiprovider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:wikimsglow/core/config/apps_config.dart';
 import 'package:wikimsglow/core/routes/router.gr.dart';
 import 'package:wikimsglow/core/theme/my_themes.dart';
+import 'package:wikimsglow/features/login/presentation/provider/login_notifier.dart';
+import 'package:wikimsglow/injection.dart' as di;
 
 void main() {
+  di.init();
   runApp(AppWidget());
 }
 
@@ -14,11 +18,16 @@ class AppWidget extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: AppsConfig.appTitle,
-      theme: myThemes(),
-      routerDelegate: _appRouter.delegate(),
-      routeInformationParser: _appRouter.defaultRouteParser(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => di.locator<LoginNotifier>()),
+      ],
+      child: MaterialApp.router(
+        title: AppsConfig.appTitle,
+        theme: myThemes(),
+        routerDelegate: _appRouter.delegate(),
+        routeInformationParser: _appRouter.defaultRouteParser(),
+      ),
     );
   }
 }
